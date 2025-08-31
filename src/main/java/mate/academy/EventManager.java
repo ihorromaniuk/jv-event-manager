da70711248686009c017beffec419f5c60e46500
Ihor Romaniuk <ihor.rome@gmail.com>
2025-08-31 15:02:45 +0300

    fix

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -1,6 +1,7 @@
 package mate.academy;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -10,16 +11,31 @@ public class EventManager {
     private final ExecutorService executorService = Executors.newCachedThreadPool();
 
     public void registerListener(EventListener listener) {
-        listeners.add(listener);
+        if (Objects.nonNull(listener) && !executorService.isShutdown()) {
+            listeners.add(listener);
+        }
     }
 
     public void deregisterListener(EventListener listener) {
-        listeners.remove(listener);
+        if (Objects.nonNull(listener) && !executorService.isShutdown()) {
+            listeners.remove(listener);
+        }
     }
 
     public void notifyEvent(Event event) {
-        for (EventListener listener : listeners) {
-            executorService.submit(() -> listener.onEvent(event));
+        if (Objects.nonNull(event) && !executorService.isShutdown()) {
+            for (EventListener listener : listeners) {
+                executorService.submit(() -> {
+                    try {
+                        listener.onEvent(event);
+                    } catch (Exception e) {
+                        System.out.println("Error occurred in "
+                                + Thread.currentThread().getName()
+                                + ": "
+                                + e.getMessage());
+                    }
+                });
+            }
         }
     }
 
